63a332c0e7305d9e8fb0dbe6917fdac856d7040b
Rafael Biehler <mail@rafaelbiehler.com>
2024-02-05 21:01:37 +0100

    Create wordcloud from all labels in the dataset

--- gen_wordcloud.py
+++ gen_wordcloud.py
@@ -1,13 +1,33 @@
+import os
+
+import numpy as np
 import pandas as pd
+from dotenv import load_dotenv
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import random
 
-df = pd.read_csv("./galt/person/captions.csv")
+load_dotenv()
+labels = pd.read_excel(os.environ["LABELS_FILE"])
+labels = labels.replace(np.nan, "", regex=True)
+df_unique = labels.drop_duplicates(subset="Accession No.")
 
-text = df["Caption"].values
+text = df_unique["Scope and Content"].values
 
-wordcloud = WordCloud().generate(str(text))
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(30, 70)
+
+
+wordcloud = WordCloud(margin=10, background_color="#FFFFFF", width=800, height=500).generate(str(text))
+
+default_colors = wordcloud.to_array()
+plt.imshow(wordcloud.recolor(color_func=grey_color_func, random_state=3),
+           interpolation="bilinear")
 
 plt.imshow(wordcloud)
 plt.axis("off")
+wordcloud.to_file("wordcloud.png")
 plt.show()
+
