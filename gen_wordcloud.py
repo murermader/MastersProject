98e1ee7a7a9b928d3ba2c1a5565120ac074c8a77
Rafael Biehler <mail@rafaelbiehler.com>
2024-02-09 23:34:24 +0100

    wordcloud uses same color palette as plots

--- gen_wordcloud.py
+++ gen_wordcloud.py
@@ -15,19 +15,17 @@ df_unique = labels.drop_duplicates(subset="Accession No.")
 text = df_unique["Scope and Content"].values
 
 
-def grey_color_func(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
+def grey_color_func(
+    word, font_size, position, orientation, random_state=None, **kwargs
+):
     return "hsl(0, 0%%, %d%%)" % random.randint(30, 70)
 
 
-wordcloud = WordCloud(margin=10, background_color="#FFFFFF", width=800, height=500).generate(str(text))
-
-default_colors = wordcloud.to_array()
-plt.imshow(wordcloud.recolor(color_func=grey_color_func, random_state=3),
-           interpolation="bilinear")
+wordcloud = WordCloud(
+    margin=2, colormap="viridis", background_color="#FFFFFF", width=800, height=500,
+).generate(str(text))
 
 plt.imshow(wordcloud)
 plt.axis("off")
 wordcloud.to_file("wordcloud.png")
 plt.show()
-
